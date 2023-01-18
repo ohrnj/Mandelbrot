1f646c159ac96dc54809381665c97bbc7ca7bade
Osman Hrnjica <ohrnjica@gmail.com>
2023-01-18 12:50:22 +0100

    update 4

--- app/src/main/java/icygames/com/mandelbrotfractal/Mandelbrot.kt
+++ app/src/main/java/icygames/com/mandelbrotfractal/Mandelbrot.kt
@@ -19,37 +19,35 @@ class Mandelbrot(context: Context, width: Int, height: Int, iterations: Int, pal
         iter = iterations
         mPalette = palette
 
-        // Change those values to zoom in or zoom out of Mandelbrot set,
-        // for example, set some of the values:   [or just uncomment if you are too lazy :-) ]
+        // Change these values or uncomment the ones already defined to zoom into the Mandelbrot set,
+        // for example, set some of the values:
 
         // default value:
         zoom = 1.0; positionX = 0.0; positionY = 0.0; iter = 100
-
-//         zoom = 0.6 ;  positionX = 0.4; ;  positionY = 0.4 ;  iter = 100
-//         zoom = 0.1 ;  positionX = 0.9 ;  positionY = 0.9 ;  iter = 100
-//         zoom = 0.03 ;  positionX = 0.97 ;  positionY = 0.97 ;  iter = 150
-//         zoom = 0.009 ;  positionX = 1.006 ;  positionY = 0.991 ;  iter = 200
-//         zoom = 0.008 ;  positionX = 1.09 ;  positionY = 0.992 ;  iter = 200
-//         zoom = 0.005 ;  positionX = 1.09 ;  positionY = 0.995 ;  iter = 200
-//         zoom = 0.002 ;  positionX = 1.094 ;  positionY = 0.998 ;  iter = 200
-//         zoom = 0.0006 ;  positionX = 1.098 ;  positionY = 0.9994 ;  iter = 300
-//         zoom = 0.0001 ;  positionX = 1.0987 ;  positionY = 0.9999 ;  iter = 800
-
+//          zoom = 2.0 ;  positionX = 0.5; ;  positionY = 0.5 ;  iter = 100
+//         zoom = 10.0 ;  positionX = 0.9 ;  positionY = 0.9 ;  iter = 100
+//         zoom = 30.0 ;  positionX = 0.97 ;  positionY = 0.97 ;  iter = 150
+//         zoom = 90.0 ;  positionX = 1.002 ;  positionY = 0.989 ;  iter = 200
+//         zoom = 80.0 ;  positionX = 1.09 ;  positionY = 0.9875 ;  iter = 200
+//         zoom = 180.0 ;  positionX = 1.092 ;  positionY = 0.9944 ;  iter = 200
+//         zoom = 300.0 ;  positionX = 1.092 ;  positionY = 0.9964 ;  iter = 200
+//         zoom = 600.0 ;  positionX = 1.0936 ;  positionY = 0.9984 ;  iter = 200
+//         zoom = 1200.0 ;  positionX = 1.0938 ;  positionY = 0.9992 ;  iter = 200
+//         zoom = 2000.0 ;  positionX = 1.0945 ;  positionY = 0.99954 ;  iter = 200
+//         zoom = 3000.0 ;  positionX = 1.0948 ;  positionY = 0.99968 ;  iter = 600
+//         zoom = 5000.0 ;  positionX = 1.09505 ;  positionY = 0.99980 ;  iter = 1600
     }
 
     fun generate(bitmap: IntArray) {
-        val xS = (MAXX - MINX) / mWidth
-        val yS = (MAXY - MINY) / mHeight
 
         var y = 0
         while (y < mHeight) {
-            val cY = MINY + y * yS * zoom
+            val cY = -1.0 + y * (2.0/mHeight) / zoom
             var x = 0
             while (x < mWidth) {
-                val cX = MINX + x * xS * zoom
-                var colorIndex = calculateMandelbrot(cX, cY) * (mPalette.size - 1) / iter
-                if (colorIndex < 0) colorIndex = 0
+                val cX = -2.5 + x * (3.5/mWidth) / zoom
 
+                val colorIndex = calculateMandelbrot(cX, cY) * (mPalette.size - 1) / iter
                 bitmap[y * mWidth + x] = mPalette[colorIndex]
                 x++
             }
@@ -61,9 +59,9 @@ class Mandelbrot(context: Context, width: Int, height: Int, iterations: Int, pal
         var zx = 0.0
         var zy = 0.0
         var i = 0
-        while (i < iter && (zx * zx + zy * zy) < END) {
+        while (i < iter && (zx * zx + zy * zy) < 4) {
             val zxtmp = zx * zx - zy * zy + cX + positionX
-            zy = START * zx * zy + cY + positionY
+            zy = 2 * zx * zy + cY + positionY
             zx = zxtmp
             i++
         }
@@ -73,14 +71,5 @@ class Mandelbrot(context: Context, width: Int, height: Int, iterations: Int, pal
     fun setDimensions(width: Int, height: Int) {
         mWidth = width
         mHeight = height
-    }
-
-    companion object {
-        private const val MINX = -2.5
-        private const val MAXX = 1.0
-        private const val MINY = -1.0
-        private const val MAXY = 1.0
-        private const val START = 2.0
-        private const val END = START * START
     }
 }
